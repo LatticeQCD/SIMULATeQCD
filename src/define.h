fc872e6656fa78743c1044bc504062de5acba3bf
Lukas Mazur <lukas.mazur@uni-paderborn.de>
2021-12-04 22:23:01 +0100

    fixed missing parentheses

--- src/define.h
+++ src/define.h
@@ -19,12 +19,9 @@
 #define RET0_IF_SCALAR
 #define RETa_IF_SCALAR
 #else
-#define EMPTY_IF_SCALAR                                                        \
-  {}
-#define RET0_IF_SCALAR                                                         \
-  { return 0; }
-#define RETa_IF_SCALAR                                                         \
-  { return a; }
+#define EMPTY_IF_SCALAR {}
+#define RET0_IF_SCALAR { return 0; }
+#define RETa_IF_SCALAR { return a; }
 #endif
 
 
@@ -68,35 +65,33 @@ const HaloSegment HaloStripes[] = {HaloSegment::XYZ, HaloSegment::XYT, HaloSegme
 const HaloSegment HaloCorners[] = {HaloSegment::XYZT};
 
 inline int HaloSegmentDirections(HaloSegment seg) {
-  if (seg == X || seg == Y || seg == Z || seg == T)
-    return 1;
-  else if (seg == XY || seg == XZ || seg == XT || seg == YZ || seg == YT ||
-           seg == ZT)
-    return 2;
-  else if (seg == XYZ || seg == XYT || seg == XZT || seg == YZT)
-    return 4;
-  else
-    return 8;
+    if (seg == X || seg == Y || seg == Z || seg == T) return 1;
+    else if (seg == XY || seg == XZ || seg == XT || seg == YZ || seg == YT || seg == ZT) return 2;
+    else if (seg == XYZ || seg == XYT || seg == XZT || seg == YZT) return 4;
+    else return 8;
 }
 
 inline int haloSegmentCoordToIndex(const HaloSegment halseg, const size_t direction,const int leftRight) {
-  int index = 0;
-  if (halseg < 4) {
-    index = halseg * 2 + leftRight;
-  } else if (halseg < 10) {
-    size_t fakt = 2 * leftRight + direction;
-    fakt = fakt < 2 ? fakt : 5 - fakt;
-    index = 8 + ((size_t)halseg - 4) * 4 + fakt;
-  } else if (halseg < 14) {
-    size_t fakt = 4 * leftRight + direction;
-    fakt = fakt < 4 ? fakt : 11 - fakt;
-    index = 8 + 24 + ((size_t)halseg - 10) * 8 + fakt;
-  } else {
-    size_t fakt = 8 * leftRight + direction;
-    fakt = fakt < 8 ? fakt : 23 - fakt;
-    index = 8 + 24 + 32 + ((size_t)halseg - 14) * 16 + fakt;
-  }
-  return index;
+        int index = 0;
+        if (halseg < 4){
+            index = halseg * 2 + leftRight;
+        }
+        else if (halseg < 10) {
+            size_t fakt = 2*leftRight + direction;
+            fakt = fakt < 2 ? fakt : 5- fakt;
+            index = 8+((size_t) halseg - 4) * 4 + fakt;
+        }
+        else if (halseg < 14){
+            size_t fakt = 4*leftRight + direction;
+            fakt = fakt < 4 ? fakt : 11 - fakt;
+            index = 8+24+((size_t) halseg - 10) * 8 + fakt;
+        }
+        else {
+            size_t fakt = 8*leftRight + direction;
+            fakt = fakt < 8 ? fakt : 23 - fakt;
+            index =8+24+32+((size_t) halseg - 14) * 16 + fakt;
+        }
+        return index ;
 }
 
 
@@ -109,66 +104,53 @@ public:
         else if (N < 32) return Plane;
         else if (N < 64) return Stripe;
         else return Corner;
-}
 
-constexpr HaloSegment haloSeg() {
-  if (haloType() == Hyperplane)
-    return (HaloSegment)(N / 2);
-  else if (haloType() == Plane)
-    return (HaloSegment)(4 + (N - 8) / 4);
-  else if (haloType() == Stripe)
-    return (HaloSegment)(10 + (N - 32) / 8);
-  else
-    return (HaloSegment)XYZT; // Corner
-}
+    }
 
-constexpr int subIndex() {
-  if (haloType() == Hyperplane)
-    return N % 2;
-  else if (haloType() == Plane)
-    return (N - 8) % 4;
-  else if (haloType() == Stripe)
-    return (N - 32) % 8;
-  else
-    return (N - 64); // Corner
-}
+    constexpr HaloSegment haloSeg() {
+        if (haloType() == Hyperplane) return (HaloSegment) (N / 2);
+        else if (haloType() == Plane) return (HaloSegment) (4 + (N - 8) / 4);
+        else if (haloType() == Stripe) return (HaloSegment) (10 + (N - 32) / 8);
+        else return (HaloSegment) XYZT; // Corner
+    }
 
-constexpr int dir() {
-  if (haloType() == Hyperplane)
-    return 0;
-  else if (haloType() == Plane)
-    return ((subIndex() + 1) % 4) / 2;
-  else if (haloType() == Stripe)
-    return (subIndex() < 4 ? subIndex() : 3 - (subIndex() % 4));
-  else
-    return subIndex() < 8 ? subIndex() : 7 - (subIndex() % 8); // Corner
-}
+    constexpr int subIndex() {
+        if (haloType() == Hyperplane) return N % 2;
+        else if (haloType() == Plane) return (N - 8) % 4;
+        else if (haloType() == Stripe) return (N - 32) % 8;
+        else return (N - 64); // Corner
+    }
 
-constexpr int leftRight() {
-  if (haloType() == Hyperplane)
-    return N % 2;
-  else if (haloType() == Plane)
-    return ((N - 8) % 4) >= 2;
-  else if (haloType() == Stripe)
-    return ((N - 32) % 8) >= 4;
-  else
-    return (N - 64) >= 8; // Corner
-}
-}
-;
+    constexpr int dir() {
+        if (haloType() == Hyperplane) return 0;
+        else if (haloType() == Plane) return ((subIndex() + 1) % 4) / 2;
+        else if (haloType() == Stripe) return (subIndex() < 4 ? subIndex() : 3 - (subIndex() % 4));
+        else return subIndex() < 8 ? subIndex() : 7 - (subIndex() % 8); // Corner
+
+
+    }
+
+    constexpr int leftRight() {
+        if (haloType() == Hyperplane) return N % 2;
+        else if (haloType() == Plane) return ((N - 8) % 4) >= 2;
+        else if (haloType() == Stripe) return ((N - 32) % 8) >= 4;
+        else return (N - 64) >= 8; // Corner
+    }
+};
 
 namespace CoutColors {
-const std::string red("\033[0;31m");
-const std::string redBold("\033[1;31m");
-const std::string green("\033[0;32m");
-const std::string greenBold("\033[1;32m");
-const std::string yellow("\033[0;33m");
-const std::string yellowBold("\033[1;33m");
-const std::string cyan("\033[0;36m");
-const std::string cyanBold("\033[1;36m");
-const std::string magenta("\033[0;35m");
-const std::string magentaBold("\033[1;35m");
-const std::string reset("\033[0m");
-} // namespace CoutColors
+    const std::string red("\033[0;31m");
+    const std::string redBold("\033[1;31m");
+    const std::string green("\033[0;32m");
+    const std::string greenBold("\033[1;32m");
+    const std::string yellow("\033[0;33m");
+    const std::string yellowBold("\033[1;33m");
+    const std::string cyan("\033[0;36m");
+    const std::string cyanBold("\033[1;36m");
+    const std::string magenta("\033[0;35m");
+    const std::string magentaBold("\033[1;35m");
+    const std::string reset("\033[0m");
+}
 
-#endif // GPU_LATTICE_DEFINE_H
+
+#endif //GPU_LATTICE_DEFINE_H
