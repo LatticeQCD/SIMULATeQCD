05e7315659a06c41ec693800b53741df2fc79ca1
lukas-mazur <59335056+lukas-mazur@users.noreply.github.com>
2022-01-17 02:04:25 +0100

    fixed further merge conflicts in staggeredPhases

--- src/modules/HISQ/staggeredPhases.h
+++ src/modules/HISQ/staggeredPhases.h
@@ -7,7 +7,6 @@
 #include "../../base/indexer/BulkIndexer.h"
 
 
-
 struct calcStaggeredPhase {
     inline __host__ __device__ int operator()(const gSiteMu &siteMu) const {
 
@@ -54,7 +53,6 @@ template <class floatT>
 struct imagMuphase {
     inline __host__ __device__ GPUcomplex<floatT> operator()(const gSiteMu &siteMu, double chmp) const {
 
-        typedef GIndexer<All> GInd;
         GPUcomplex<floatT> img_chmp;
 
 
@@ -74,32 +72,5 @@ struct imagMuphase {
         return 1;
     }
 };
-
-
-template<class floatT, bool onDevice, size_t HaloDepth, CompressionType comp>
-struct staggeredPhaseKernel {
-
-    //Member variables to hold all information needed
-    gaugeAccessor<floatT, comp> gAcc;
-
-    calcStaggeredPhase staggPhase;
-    calcStaggeredBoundary staggBound;
-    imagMuphase<floatT> imaginaryPhase;
-    double mu_f;
-
-    //Constructor to initialize this member variable.
-    explicit staggeredPhaseKernel(Gaugefield<floatT, onDevice, HaloDepth, comp> &gaugeIn, floatT _mu_f=0.0) :
-            gAcc(gaugeIn.getAccessor()),mu_f(_mu_f) {}
-
-    __host__ __device__ GSU3<floatT> operator()(gSiteMu siteMu) {
-        GCOMPLEX(floatT) phase =1.0;
-        if (mu_f == 0 )
-            phase = staggPhase(siteMu) * staggBound(siteMu);
-        else
-            phase = staggPhase(siteMu) * staggBound(siteMu) * imaginaryPhase(siteMu, mu_f);
-        return phase * gAcc.getLink(siteMu);
-    }
-};
-
 
 #endif //STAGGEREDPHASES_H
--- src/modules/HISQ/staggeredPhasesKernel.h
+++ src/modules/HISQ/staggeredPhasesKernel.h
@@ -1,8 +1,8 @@
 //
 // Created by Lukas Mazur on 04.01.19.
 //
-#ifndef STAGGEREDPHASES_H
-#define STAGGEREDPHASES_H
+#ifndef STAGGEREDPHASES_KERNEL_H
+#define STAGGEREDPHASES_KERNEL_H
 
 #include "../../gauge/gaugefield.h"
 #include "staggeredPhases.h"
@@ -15,13 +15,19 @@ struct staggeredPhaseKernel {
 
     calcStaggeredPhase staggPhase;
     calcStaggeredBoundary staggBound;
+    imagMuphase<floatT> imaginaryPhase;
+    double mu_f;
 
     //Constructor to initialize this member variable.
-    explicit staggeredPhaseKernel(Gaugefield<floatT, onDevice, HaloDepth, comp> &gaugeIn) :
-            gAcc(gaugeIn.getAccessor()) {}
+    explicit staggeredPhaseKernel(Gaugefield<floatT, onDevice, HaloDepth, comp> &gaugeIn, floatT _mu_f=0.0) :
+            gAcc(gaugeIn.getAccessor()),mu_f(_mu_f) {}
 
     __host__ __device__ GSU3<floatT> operator()(gSiteMu siteMu) {
-        floatT phase = staggPhase(siteMu) * staggBound(siteMu);
+        GCOMPLEX(floatT) phase =1.0;
+        if (mu_f == 0 )
+            phase = staggPhase(siteMu) * staggBound(siteMu);
+        else
+            phase = staggPhase(siteMu) * staggBound(siteMu) * imaginaryPhase(siteMu, mu_f);
         return phase * gAcc.getLink(siteMu);
     }
 };
