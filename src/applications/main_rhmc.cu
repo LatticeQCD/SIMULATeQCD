1a6658a2b42ab062c1d0883f4d00e8eb4fbbd8aa
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-01-28 12:28:29 +0100

    remove command line argument parsing for RationalCoeff class

--- src/applications/main_rhmc.cu
+++ src/applications/main_rhmc.cu
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 
     RationalCoeff rat;
 
-    rat.readfile(commBase, param.rat_file(), 0, NULL);
+    rat.readfile(commBase, param.rat_file());
 
     rat.check_rat(param);
 
