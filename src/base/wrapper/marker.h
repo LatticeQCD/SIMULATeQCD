ac78962fd2d849e1bea3ef06c85bac22c5b5f3c8
Dennis Bollweg <dbollweg@physik.uni-bielefeld.de>
2024-02-23 16:28:22 -0500

    Add AutoTimer to kernel launches

--- src/base/wrapper/marker.h
+++ src/base/wrapper/marker.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <string_view>
 #include "gpu_wrapper.h"
+#include "../stopWatch.h"
+#include "../utilities/parseObjectName.h"
 
 inline void markerBegin(std::string_view marker_name, std::string_view group_name){
 #ifdef USE_MARKER
@@ -25,3 +27,25 @@ inline void markerEnd(){
     #endif
 #endif
 }
+
+template<bool onDevice>
+class AutoTimer {
+    StopWatch<onDevice> internalTimer;
+    std::string_view marker_name;
+    std::string_view group_name;
+    public:
+    AutoTimer<onDevice>(std::string_view mn, std::string_view gn) : marker_name(mn), group_name(gn) {
+        if (rootLogger.getVerbosity() == TRACE) {
+        internalTimer.start();
+        markerBegin(marker_name, group_name);
+        }
+    }
+
+    ~AutoTimer<onDevice>(){
+        if (rootLogger.getVerbosity() == TRACE) {
+        markerEnd();
+        internalTimer.stop();
+        rootLogger.trace(std::string(group_name), ": ", std::string(marker_name), "took ", internalTimer);
+        }
+    }
+};
