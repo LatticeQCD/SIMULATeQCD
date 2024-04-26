71cef83ed8beddda7b9e4e70a4f893e8e0411a68
Dennis Bollweg <dbollweg@physik.uni-bielefeld.de>
2024-04-26 16:58:39 -0400

    Add iterateWithConstObj and iterateFunctorLoop to sycl version of RunFunctors

--- src/base/runFunctors_sycl.h
+++ src/base/runFunctors_sycl.h
@@ -15,23 +15,45 @@ template<bool onDevice, class Accessor>
 class RunFunctors {
     public:
     virtual Accessor getAccessor() const = 0;
-/*
+
     template<unsigned WgSize = DEFAULT_NBLOCKS_CONST, typename CalcReadInd, typename CalcWriteInd, typename Object>
-    void iterateWithConstObject(Object ob, CalcReadInd calcReadInd, CalcWriteInd, calcWriteInd,
-    const size_t elems_x, const size_t elems_y = 1, const size_t elems_z = 1, sycl::queue q = default_queue);
-*/
+    void iterateWithConstObject(Object ob, CalcReadInd calcReadInd, CalcWriteInd calcWriteInd,
+    const size_t elems_x, sycl::queue q);
+
     template<unsigned WgSize = DEFAULT_NBLOCKS, typename CalcReadInd, typename CalcWriteInd, typename Functor>
     void iterateFunctor(Functor Op, CalcReadInd calcReadInd, CalcWriteInd calcWriteInd, 
     const size_t elems_x, sycl::queue q);
 
-/*
+
     template<size_t Nloops, unsigned WgSize = DEFAULT_NBLOCKS_LOOP, typename CalcReadInd, typename CalcWriteInd, typename Functor>
     void iterateFunctorLoop(Functor op, CalcReadInd calcReadInd, CalcWriteInd calcWriteInd, 
-    const size_t elems_x, const size_t elems_y = 1, const size_t elems_z =1, sycl::queue q = default_queue);
-*/
-    private:
-    sycl::queue default_queue;
+    const size_t elems_x, sycl::queue q, size_t Nmax = Nloops);
+
 };
+
+template<bool onDevice, class Accessor>
+template<unsigned WgSize, typename CalcReadInd, typename CalcWriteInd, typename Object>
+void RunFunctors<onDevice, Accessor>::iterateWithConstObject(Object ob, CalcReadInd calcReadInd, CalcWriteInd calcWriteInd,
+    const size_t elems_x, sycl::queue q) {
+
+    q.submit([&] (sycl::handler& cgh) {
+
+        auto iteration_range = sycl::nd_range{sycl::range<1>{elems_x}, sycl::range<1>{DEFAULT_NBLOCKS_CONST}};
+        auto acc = getAccessor();
+
+        cgh.parallel_for(iteration_range, [=](sycl::nd_item<1> itm) {
+            size_t i = itm.get_local_id()+itm.get_group().get_group_id(0)*itm.get_local_range(0);
+
+            auto site = calcReadInd(i);
+            acc.setElement(CalcWriteInd(site), ob);
+        });
+
+    }).wait();
+
+
+}
+
+
 
 template<bool onDevice, class Accessor>
 template<unsigned WgSize, typename CalcReadInd, typename CalcWriteInd, typename Functor>
@@ -40,16 +62,44 @@ void RunFunctors<onDevice, Accessor>::iterateFunctor(Functor op, CalcReadInd cal
     
 
     q.submit([&] (sycl::handler& cgh) {
+
         auto iteration_range = sycl::nd_range{sycl::range<1> {elems_x}, sycl::range<1>{DEFAULT_NBLOCKS}};
-        auto acc = getAccessor();
+        auto acc = getAccessor(); //implicit capture of 'this' is not allowed inside sycl kernel so we get hold of a local copy here
+
         cgh.parallel_for(iteration_range, [=](sycl::nd_item<1> itm) {
             size_t i = itm.get_local_id()+itm.get_group().get_group_id(0)*itm.get_local_range(0);
             
             auto site = calcReadInd(i);
             acc.setElement(calcWriteInd(site), op(site));
-            // performFunctor(getAccessor(), op, calcReadInd, calcWriteInd, elems_x);
         });
-    });
+
+    }).wait();
+}
+
+template<bool onDevice, class Accessor>
+template<size_t Nloops, unsigned WgSize, typename CalcReadInd, typename CalcWriteInd, typename Functor>
+void RunFunctors<onDevice, Accessor>::iterateFunctorLoop(Functor op, CalcReadInd calcReadInd,
+CalcWriteInd calcWriteInd, const size_t elems_x, sycl::queue q, size_t Nmax) {
+    
+    if (Nmax > Nloops) {
+        throw std::runtime_error(stdLogger.fatal("Nmax larger than Nloops!"));
+    }
+
+    q.submit([&] (sycl::handler& cgh) {
+        auto iteration_range = sycl::nd_range{sycl::range<1> {elems_x}, sycl::range<1>{DEFAULT_NBLOCKS}};
+        auto acc = getAccessor();
+
+        cgh.parallel_for(iteration_range, [=] (sycl::nd_item<1> itm) {
+            size_t i = itm.get_local_id() + itm.get_group().get_group_id(0)*itm.get_local_range(0);
+
+            auto site = calcReadInd(i);
+            op.initialize(site);
+
+            for (size_t loopIdx = 0; loopIdx < Nloops; loopIdx++) {
+                acc.setElement(calcWriteInd(site, loopIdx), op(site, loopIdx));
+            }
+        });
+    }).wait();
 }
 
 template<Layout LatticeLayout, size_t HaloDepth>
@@ -67,21 +117,4 @@ struct WriteAtRead {
         return site;
     }
 };
-// template<typename Accessor, typename Functor, typename CalcReadInd, typename CalcWriteInd>
-// void performFunctor(Accessor res, Functor op, CalcReadInd calcReadInd, CalcWriteInd calcWriteInd, const size_t size_x) {
 
-//     // size_t i = blockDim.x * blockIdx.x + threadIdx.x;
-//     size_t i = 
-//     if (i >= size_x) {
-//         return;
-//     }
-
-//     //Site can be anything. Therefore auto
-// #ifdef USE_CUDA
-//     auto site = calcReadInd(blockDim, blockIdx, threadIdx);
-// #elif defined USE_HIP
-// auto site = calcReadInd(dim3(blockDim), GetUint3(dim3(blockIdx)), GetUint3(dim3(threadIdx)));
-// #endif
-//     auto site = calcReadInd()
-//     res.setElement(calcWriteInd(site), op(site));
-// }
