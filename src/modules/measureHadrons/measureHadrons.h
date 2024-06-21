968d3e67176c6891af4fbdbeeb17044e9ccf2dea
Tristan <trisueding@web.de>
2024-06-21 13:34:05 +0200

    Nest try

--- src/modules/measureHadrons/measureHadrons.h
+++ src/modules/measureHadrons/measureHadrons.h
@@ -202,7 +202,12 @@ public:
             default:
                 throw std::runtime_error(stdLogger.fatal("Unknown correlator axis! Choose one from {x, y, z, t}."));
         }
-            _axes_indices.insert( _axes_indices.begin() + 4 * i, _tmp_axis_indices , _tmp_axis_indices + 4 ) ;
+            for (size_t j = 0; j < 4; j++)
+            {
+                _axes_indices[4 * i + j] = _tmp_axis_indices[j] ;// there shouldt be a better way to do this maybe with vector.insert ?
+            }
+            
+            //_axes_indices.insert( _axes_indices.begin() + 4 * i, _tmp_axis_indices , _tmp_axis_indices + 4 ) ;
         }
         
         for (size_t i = 0; i < _axes_indices.size(); i++)
