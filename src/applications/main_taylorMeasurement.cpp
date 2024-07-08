16ae5a2d2e31a6397fb9c235df7c8319255529b2
Finn Daut <finn.daut@gmail.com>
2024-07-09 00:29:10 +0200

    modified:   src/applications/main_taylorMeasurement.cpp

--- src/applications/main_taylorMeasurement.cpp
+++ src/applications/main_taylorMeasurement.cpp
@@ -26,9 +26,9 @@ int main(int argc, char **argv) {
 
     commBase.init(param.nodeDim());
 
-    const int HaloDepth = 2; // >= 1 for multi gpu
-    const int HaloDepthSpin = 4;
-    const int NStacks = 1; // NOTE: this only works for NStacks=8 after the blocksize fix
+    const size_t HaloDepth = 2; // >= 1 for multi gpu
+    const size_t HaloDepthSpin = 4;
+    const size_t NStacks = 1; // NOTE: this only works for NStacks=8 after the blocksize fix
     typedef float floatT; // Define the precision here
     typedef float PREC;
 
@@ -49,17 +49,17 @@ int main(int argc, char **argv) {
     // const int sizeh = param.latDim[0]*param.latDim[1]*param.latDim[2]*param.latDim[3]/2;
 
     // // Read the configuration. Remember a halo exchange is needed every time the gauge field changes.
-    Gaugefield<floatT,true,HaloDepth,R14> gauge(commBase);  
+    Gaugefield<floatT,true,HaloDepth,R18> gauge(commBase);  
     
     // naik_epsilon(use_naik_epsilon ? get_naik_epsilon_from_amc(mass) : 0.0);  /// gauge field
     rootLogger.info("Read configuration from ", param.GaugefileName());
     gauge.readconf_nersc(param.GaugefileName());
     gauge.updateAll();
 
-    Gaugefield<floatT,true,HaloDepth,R18> gauge_smeared(commBase);
-    Gaugefield<floatT,true,HaloDepth,U3R14> gauge_Naik(commBase);
-    HisqSmearing<floatT, true, HaloDepth> smearing(gauge, gauge_smeared, gauge_Naik);
-    smearing.SmearAll();
+    // Gaugefield<floatT,true,HaloDepth,R18> gauge_smeared(commBase);
+    // Gaugefield<floatT,true,HaloDepth,U3R14> gauge_Naik(commBase);
+    // HisqSmearing<floatT, true, HaloDepth> smearing(gauge, gauge_smeared, gauge_Naik);
+    // smearing.SmearAll();
     
     // Read the Eigenvalues and Eigenvectors
     eigenpairs<PREC,true,Even,HaloDepthSpin,NStacks> eigenpairs(commBase);
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
     eigenpairs.read_evnersc(param.num_toread_vectors(), param.eigen_file());
     eigenpairs.updateAll();
 
-    HisqDSlash<floatT,true,Even,HaloDepth,HaloDepthSpin,NStacks> dslash(gauge_smeared, gauge_Naik, 0.0);
+    // HisqDSlash<floatT,true,Even,HaloDepth,HaloDepthSpin,NStacks> dslash(gauge_smeared, gauge_Naik, 0.0);
 
     // eigenpairs.tester(dslash, param.num_toread_vectors());
 
@@ -91,28 +91,28 @@ int main(int argc, char **argv) {
 
     for (double mass : param.valence_masses.get()) {
         rootLogger.info("Using mass ", mass);
+        
+        TaylorMeasurement<floatT, true, HaloDepth, HaloDepthSpin, NStacks> taylor_measurement(gauge, param, mass, param.use_naik_epsilon(), d_rand);
+        try {
+            for (const auto& id : param.operator_ids.get()) {
+                taylor_measurement.insertOperator(id);
+            }
+        }
+        catch (std::runtime_error e) {
+            rootLogger.error(e.what());
+            return 1;
+        }
+        rootLogger.info("Operators added");
+        taylor_measurement.write_output_file_header();
+        taylor_measurement.computeOperators();
+        rootLogger.info("Operators computed");
 
-        TaylorMeasurement<PREC, true, HaloDepth, HaloDepthSpin, NStacks> taylor_measurement(gauge, param, mass, param.use_naik_epsilon(), d_rand);
-    //     try {
-    //         for (const auto& id : param.operator_ids.get()) {
-    //             taylor_measurement.insertOperator(id);
-    //         }
-    //     }
-    //     catch (std::runtime_error e) {
-    //         rootLogger.error(e.what());
-    //         return 1;
-    //     }
-    //     rootLogger.info("Operators added");
-    //     taylor_measurement.write_output_file_header();
-    //     taylor_measurement.computeOperators();
-    //     rootLogger.info("Operators computed");
-
-    //     std::vector<DerivativeOperatorMeasurement> results;
-    //     taylor_measurement.collectResults(results);
-    //     rootLogger.info("Results collected");
-    //     for (DerivativeOperatorMeasurement &meas : results) {
-    //         rootLogger.info("ID: ", meas.operatorId, ", Measurement: ", meas.measurement, ", Error: ", meas.std);
-    //     }
+        std::vector<DerivativeOperatorMeasurement> results;
+        taylor_measurement.collectResults(results);
+        rootLogger.info("Results collected");
+        for (DerivativeOperatorMeasurement &meas : results) {
+            rootLogger.info("ID: ", meas.operatorId, ", Measurement: ", meas.measurement, ", Error: ", meas.std);
+        }
         
     }
 
